978248f304f6b1ab6e4a91c3f599b89a20f03abf
alex blade <aalexblade@gmail.com>
2026-09-10 22:05:44 +0300

    fix(shared): correct gsap plugin registration

--- src/shared/lib/gsap/index.ts
+++ src/shared/lib/gsap/index.ts
@@ -2,9 +2,8 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-// Перевірка на браузерне середовище для безпечної роботи з Next.js SSR
 if (typeof window !== 'undefined') {
-  gsap.registerPlugin(ScrollTrigger, useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
 }
 
 export { gsap, ScrollTrigger, useGSAP };
