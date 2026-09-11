406e1dbf46de36c58dc47c5e8c6329d564a949bd
alex blade <aalexblade@gmail.com>
2026-09-11 20:31:19 +0300

    feat(home): mount CoffeeStory scrollytelling widget on HomePage

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -4,14 +4,12 @@ import { Hero } from '@/widgets/hero';
 
 export function HomePage() {
   return (
-    <>
+    <main className="flex-1">
       <Header />
-
-      <main>
-        <Hero />
-
-        <CoffeeStory />
-      </main>
-    </>
+      <Hero />
+      <CoffeeStory />
+      {/* Інші секції будуть нижче */}
+      <div style={{ height: '100vh', backgroundColor: '#1a100c' }} />
+    </main>
   );
 }
