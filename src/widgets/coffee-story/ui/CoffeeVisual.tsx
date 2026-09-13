497585b0689969be9d6c1587218a53bc337c6731
alex blade <aalexblade@gmail.com>
2026-09-13 19:18:35 +0300

    refactor(coffee-story): update visual component handle to proxy getters directly

--- src/widgets/coffee-story/ui/CoffeeVisual.tsx
+++ src/widgets/coffee-story/ui/CoffeeVisual.tsx
@@ -11,42 +11,55 @@ export const CoffeeVisual = forwardRef<CoffeeVisualHandle>((_, ref) => {
       get root() {
         return sceneRef.current?.root ?? null;
       },
+
       get hero() {
         return sceneRef.current?.hero ?? null;
       },
+
       get package() {
         return sceneRef.current?.package ?? null;
       },
+
       get beans() {
         return sceneRef.current?.beans ?? null;
       },
+
       get espresso() {
         return sceneRef.current?.espresso ?? null;
       },
+
       get espressoCup() {
         return sceneRef.current?.espressoCup ?? null;
       },
+
       get espressoLiquid() {
         return sceneRef.current?.espressoLiquid ?? null;
       },
+
       get espressoCrema() {
         return sceneRef.current?.espressoCrema ?? null;
       },
+
       get espressoStream() {
         return sceneRef.current?.espressoStream ?? null;
       },
+
       get cortado() {
         return sceneRef.current?.cortado ?? null;
       },
+
       get cortadoGlass() {
         return sceneRef.current?.cortadoGlass ?? null;
       },
+
       get cortadoLiquid() {
         return sceneRef.current?.cortadoLiquid ?? null;
       },
+
       get cortadoMilk() {
         return sceneRef.current?.cortadoMilk ?? null;
       },
+
       get cortadoStream() {
         return sceneRef.current?.cortadoStream ?? null;
       },
@@ -54,11 +67,7 @@ export const CoffeeVisual = forwardRef<CoffeeVisualHandle>((_, ref) => {
     [],
   );
 
-  return (
-    <div>
-      <CoffeeScene ref={sceneRef} />
-    </div>
-  );
+  return <CoffeeScene ref={sceneRef} />;
 });
 
 CoffeeVisual.displayName = 'CoffeeVisual';
