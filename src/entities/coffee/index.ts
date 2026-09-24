25e253c66777973676fe403a68d8ae653d0cf185
alex blade <aalexblade@gmail.com>
2026-09-24 22:48:00 +0300

    feat(coffee): update public API export

--- src/entities/coffee/index.ts
+++ src/entities/coffee/index.ts
@@ -1,2 +1,3 @@
-export type { Coffee, CoffeeAccent } from './model/coffee.types';
+export * from './model/coffee.types';
 export { COFFEE_MENU } from './config/coffeeMenu';
+export { fetchCoffeeMenu } from './api/fetchCoffeeMenu';
