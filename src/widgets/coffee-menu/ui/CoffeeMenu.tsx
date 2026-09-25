654100a087d37e6f7e8f7e310ee30c25f556ea56
alex blade <aalexblade@gmail.com>
2026-09-25 20:48:36 +0300

    fix(coffee-menu): update CoffeeMenu to async Server Component using fetchCoffeeMenu

--- src/widgets/coffee-menu/ui/CoffeeMenu.tsx
+++ src/widgets/coffee-menu/ui/CoffeeMenu.tsx
@@ -1,9 +1,11 @@
+import { fetchCoffeeMenu, type Coffee } from '@/entities/coffee';
 import { Container } from '@/shared/ui';
-import { COFFEE_MENU } from '@/entities/coffee';
 import { CoffeeMenuCard } from './CoffeeMenuCard';
 import styles from './CoffeeMenu.module.css';
 
-export function CoffeeMenu() {
+export async function CoffeeMenu() {
+  const menuItems: Coffee[] = await fetchCoffeeMenu();
+
   return (
     <section
       id="coffee-menu"
@@ -32,7 +34,7 @@ export function CoffeeMenu() {
         </div>
 
         <div className={styles.grid}>
-          {COFFEE_MENU.map((item) => (
+          {menuItems.map((item: Coffee) => (
             <CoffeeMenuCard key={item.id} item={item} />
           ))}
         </div>
